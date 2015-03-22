d9f6a042736fa77ab467a67b880681becb24236f
David Gronlund <dagronlund@gmail.com>
2015-03-21 19:23:10 -0600

    Added button functionality.

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -2,14 +2,26 @@
 
 #include "DigiJoystick.h"
 
+const int DOUT_GROUND = 6; // Just a pin that is set low to act as a ground
 const int DIN_START = 7;                     // Start of digital inputs
 const int DIN_END = 13;                      // One past the last pin index
 const int DIN_COUNT = DIN_END - DIN_START;
 
+const int DIN_B1 = 0;
+const int DIN_B2 = 1;
+const int DIN_B3 = 2;
+
 void setup() {
+  pinMode(DOUT_GROUND, OUTPUT);
+  digitalWrite(DOUT_GROUND, LOW);
+  
   for (int i = DIN_START; i < DIN_END; ++i) {
     pinMode(i, INPUT);
   }
+  
+  pinMode(DIN_B1, INPUT);
+  pinMode(DIN_B2, INPUT);
+  pinMode(DIN_B3, INPUT);
 }
 
 void loop() {
@@ -19,7 +31,12 @@ void loop() {
     byte buttonState = digitalRead(i);
     lbyte |= (buttonState << (i - DIN_START));
   }
-  
+  byte buttonState = digitalRead(DIN_B1);
+  lbyte |= (buttonState << 5);
+  buttonState = digitalRead(DIN_B2);
+  lbyte |= (buttonState << 6);
+  buttonState = digitalRead(DIN_B3);
+  lbyte |= (buttonState << 7);
   
   // Set buttons like this (lowByte, highByte)
   DigiJoystick.setButtons(lbyte, 0x00);
